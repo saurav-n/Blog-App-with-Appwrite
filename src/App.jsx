f2bf156dcba68ac36cde1d2ff7a1073eec9f8777
Saurav Niroula <sauravniroula725@gmail.com>
2024-01-15 09:31:33 +0545

    set up and configed env vars

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+  console.log(import.meta.env.VITE_APP_URL)
+  return (
+    <h1 className="bg-blue-400">Blog posting app with appwrite</h1>
+  )
+}
+
+export default App
---
+++ src/importEnvVars/importEnvVars.js
@@ -0,0 +1,7 @@
+export const envVars={
+    appUrl:String(import.meta.env.VITE_APP_URL),
+    appProjectId:String(import.meta.env.VITE_APP_PROJECTID),
+    appDBID:String(import.meta.env.VITE_APP_DBID),
+    appCollectionId:String(import.meta.env.VITE_APP_COLLECTIONID),
+    appBucketId:String(import.meta.env.VITE_APP_BUCKETID)
+}
